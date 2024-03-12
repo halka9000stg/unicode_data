576475aa110047844532ed025e5f2d31a548bdbf
佐藤陽花 <as.he.that.doth.serve@gmail.com>
2024-03-12 19:37:10 +0900

    Update unicode_scanner.dart

--- lib/src/unicode_scanner.dart
+++ lib/src/unicode_scanner.dart
@@ -2,17 +2,17 @@ import "unicode_script.dart";
 import "unicode_block.dart";
 
 extension ScriptAnalysisCP on int {
-  (String, String) scan() {
-    Iterable<(String, String)> rc = UnicodeScript().scripts.map<(String, String)?>((Script s) => (s.start <= this && this <= s.end) ? (s.name, s.category)).whereType<(String, String)>();
+  (String, String) scanScript() {
+    Iterable<(String, String)> rc = UnicodeScript().scripts.map<(String, String)?>((Script s) => (s.start <= this && this <= s.end) ? (s.name, s.category) : null).whereType<(String, String)>();
     if(rc.isEmpty){
       return ("", "");
     }
     return rc.first;
   }
-  String nameOfBlock() => this.scan().$1;
-  String categoryOfBlock() => this.scan().$2;
-  bool isInBlock({String name, String category}) {
-    Iterable<Script> sc = UnicodeScript().scripts.where((Script b) => (name == null ? true : name == b.name) && (category == null ? true : category == b.category));
+  String nameOfScript() => this.scanScript().$1;
+  String categoryOfScript() => this.scanScript().$2;
+  bool isInScript({String? name, String? category}) {
+    Iterable<Script> sc = UnicodeScript().scripts.where((Script s) => (name == null ? true : name == s.name) && (category == null ? true : category == s.category));
     if(sc.isEmpty){
       return false;
     }
@@ -20,7 +20,28 @@ extension ScriptAnalysisCP on int {
   }
 }
 extension ScriptAnalysisString on String {
-  
+  Iterable<(String, String)> scanScript() => this.runes.map<(String, String)>((int i) => i.scanScript());
+  Iterable<String> nameOfScript() => this.runes.map<String>((int i) => i.nameOfScript());
+  Iterable<String> categoryOfScript() => this.runes.map<String>((int i) => i.categoryOfScript());
+  Iterable<bool> isInScript({String? name, String? category}) => this.runes.map<bool>((int i) => i.isInScript(name: name, category: category));
 }
-extension BlockAnalysisCP on int
-extension BlockAnalysisString on String {}
+extension BlockAnalysisCP on int{
+  String scanBlock() {
+    Iterable<String> rc = UnicodeBlock().blocks.map<String?>((Block b) => (b.start <= this && this <= b.end) ? b.name : null).whereType<String>();
+    if(rc.isEmpty){
+      return "";
+    }
+    return rc.first;
+  }
+  bool isInBlock({String? name}){
+    Iterable<Block> bc = UnicodeBlock().blocks.where((Block b) => name == null ? true : name == b.name);
+    if(bc.isEmpty){
+      return false;
+    }
+    return bc.first.start <= this && this <= bc.first.end;
+  }
+}
+extension BlockAnalysisString on String {
+  Iterable<String> scanBlock() => this.runes.map<String>((int i) => i.scanBlock());
+  Iterable<bool> isInBlock({String? name}) => this.r
+}
